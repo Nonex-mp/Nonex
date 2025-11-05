9b5b818699b21fa633821fb3e7f6145a2f9753cb
Mark Pacurib <pacuribmark99@gmail.com>
2025-11-05 23:33:38 +0800

    Fix folder structure: move CSS to static

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -24,3 +25,31 @@ def others():
 
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/presentations')
+def presentations():
+    return render_template('presentations.html')
+
+@app.route('/wallpapers')
+def wallpapers():
+    return render_template('wallpapers.html')
+
+@app.route('/clothes')
+def clothes():
+    return render_template('clothes.html')
+
+@app.route('/others')
+def others():
+    return render_template('others.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> 9d31006 (Initial commit - Nonex website)
