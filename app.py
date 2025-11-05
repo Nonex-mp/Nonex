7c4704c11d32fcf5f8c9b8288b615ff5028f7bdc
Nonex-mp <pacuribmark99@gmail.com>
2025-11-05 22:17:11 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/presentations')
+def presentations():
+    return render_template('presentations.html')
+
+@app.route('/wallpapers')
+def wallpapers():
+    return render_template('wallpapers.html')
+
+@app.route('/clothes')
+def clothes():
+    return render_template('clothes.html')
+
+@app.route('/others')
+def others():
+    return render_template('others.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
