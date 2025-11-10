fc4346acfaa65577a53709ca496ddafa308d001d
Mark Pacurib <pacuribmark99@gmail.com>
2025-11-10 13:25:57 +0800

    Added Mga Uri ng Pangungusap presentation to Filipino category last fix

--- app.py
+++ app.py
@@ -64,9 +64,8 @@ def view_presentation(subject):
         slides = [url_for('static', filename=f'presentations/mathematics{i}.png') for i in range(1, 8)]
     elif subject_lower == "filipino":
         slides = [url_for('static', filename=f'presentations/filipino{i}.png') for i in range(1, 6)]
-elif subject_lower == "mga uri ng pangungusap":
-    slides = [url_for('static', filename=f'presentations/mga_uri_ng_pangungusap{i}.png') for i in range(1, 9)]
-
+    elif subject_lower == "mga uri ng pangungusap":
+        slides = [url_for('static', filename=f'presentations/mga_uri_ng_pangungusap{i}.png') for i in range(1, 9)]
     elif subject_lower == "english":
         slides = [url_for('static', filename=f'presentations/english{i}.png') for i in range(1, 6)]
     elif subject_lower == "science":
@@ -75,6 +74,7 @@ elif subject_lower == "mga uri ng pangungusap":
         return render_template("404.html"), 404
 
     return render_template("view_presentation.html", subject=subject.capitalize(), slides=slides)
+
 
 # --- OTHER PAGES ---
 @app.route('/clothes')
